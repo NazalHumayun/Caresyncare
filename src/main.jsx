6c6f2ad2df01e2f1a9ddcb9502e5567406c9782d
Nazal Malik <mnazal250@gmail.com>
2026-05-25 16:33:37 +0500

    main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/authcontext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; 
 ReactDOM.createRoot(document.getElementById("root")).render(
